62919101b9873fdf87fdd308c0fe1231624c411b
Miguel Medeiros <31278849+miguelcmedeiros@users.noreply.github.com>
2023-09-12 15:12:07 +0100

    Cherry-pick "Export content_layers.dart (#1434)" to stable (#1436)
    Export content_layers.dart (#1434)

--- super_editor/lib/super_editor.dart
+++ super_editor/lib/super_editor.dart
@@ -15,7 +15,6 @@ export 'src/core/document_selection.dart';
 export 'src/core/edit_context.dart';
 export 'src/core/editor.dart';
 export 'src/core/styles.dart';
-
 // Super Editor
 export 'src/default_editor/attributions.dart';
 export 'src/default_editor/blockquote.dart';
@@ -26,12 +25,10 @@ export 'src/default_editor/default_document_editor.dart';
 export 'src/default_editor/default_document_editor_reactions.dart';
 export 'src/default_editor/document_caret_overlay.dart';
 export 'src/default_editor/document_focus_and_selection_policies.dart';
-export 'src/infrastructure/document_gestures.dart';
 export 'src/default_editor/document_gestures_mouse.dart';
 export 'src/default_editor/document_gestures_touch.dart';
-export 'src/infrastructure/document_gestures_interaction_overrides.dart';
-export 'src/default_editor/document_ime/document_input_ime.dart';
 export 'src/default_editor/document_hardware_keyboard/document_input_keyboard.dart';
+export 'src/default_editor/document_ime/document_input_ime.dart';
 export 'src/default_editor/horizontal_rule.dart';
 export 'src/default_editor/image.dart';
 export 'src/default_editor/layout_single_column/layout_single_column.dart';
@@ -45,15 +42,16 @@ export 'src/default_editor/tasks.dart';
 export 'src/default_editor/text.dart';
 export 'src/default_editor/text_tools.dart';
 export 'src/default_editor/unknown_component.dart';
-
 // Document operations used by SuperEditor and/or SuperReader,
 // also made available for public use.
 export 'src/document_operations/selection_operations.dart';
-
 export 'src/infrastructure/_listenable_builder.dart';
 export 'src/infrastructure/_logging.dart';
 export 'src/infrastructure/attributed_text_styles.dart';
 export 'src/infrastructure/composable_text.dart';
+export 'src/infrastructure/content_layers.dart';
+export 'src/infrastructure/document_gestures.dart';
+export 'src/infrastructure/document_gestures_interaction_overrides.dart';
 export 'src/infrastructure/focus.dart';
 export 'src/infrastructure/ime_input_owner.dart';
 export 'src/infrastructure/keyboard.dart';
@@ -66,9 +64,8 @@ export 'src/infrastructure/scrolling_diagnostics/scrolling_diagnostics.dart';
 export 'src/infrastructure/signal_notifier.dart';
 export 'src/infrastructure/strings.dart';
 export 'src/infrastructure/super_textfield/super_textfield.dart';
-export 'src/infrastructure/touch_controls.dart';
 export 'src/infrastructure/text_input.dart';
-
+export 'src/infrastructure/touch_controls.dart';
 // Super Reader
 export 'src/super_reader/read_only_document_android_touch_interactor.dart';
 export 'src/super_reader/read_only_document_ios_touch_interactor.dart';
